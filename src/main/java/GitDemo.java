acf2ca8ce04b7b53fc19d87e4f308090c25b9139
HoneyLeeVV <941175891@qq.com>
2023-02-19 17:32:53 +0800

    first commit3

--- src/main/java/GitDemo.java
+++ src/main/java/GitDemo.java
@@ -2,5 +2,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git000!");
+        System.out.println("hello git222!");
     }
 }
